5020e98c63b08fcaf759e7b581b9884badd9ef55
ger619 <draker74@gmail.com>
2023-10-11 19:10:21 +0300

    added our first test

--- client/src/Components/test/NavBar.test.jsx
+++ client/src/Components/test/NavBar.test.jsx
@@ -5,11 +5,18 @@ describe("Navbar", () => {
     const renderNavbar = () => {
         render(<Navbar />, { wrapper: MemoryRouter });
     }
-  test("should render my component", () => {
+    test("should render my component", () => {
       // Render the Navbar component
        renderNavbar();
        // expect the navbar to be in the document
          expect(screen.getByText("Post List")).toBeInTheDocument();
-         expect(screen.getByText("New Post")).toBeInTheDocument();
-  });
+    });
+    test("should render my component", () => {
+        // Render the Navbar component
+        renderNavbar();
+        // expect the navbar to be in the document
+        expect(screen.getByText("New Post")).toBeInTheDocument();
+    });
+
+
 });
