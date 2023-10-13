4c1b43b9271fe3f4277f0021f0761ca016f3e172
ger619 <draker74@gmail.com>
2023-10-13 17:45:29 +0300

    Refactoring the post and edit form

--- client/src/Components/Posts/NewPostForm.jsx
+++ client/src/Components/Posts/NewPostForm.jsx
@@ -1,55 +1,25 @@
 // eslint-disable-next-line no-unused-vars
-import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { createPost } from "../../service/postService.js";
+import PostForm from "./PostForm.jsx";
 
 const NewPostForm = () => {
-    const [title, setTitle] = useState("");
-    const [body, setBody] = useState("");
     const navigate = useNavigate();
 
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        const post = {
-            title,
-            body,
-        };
+    const handleCreateSubmit = async (formData) => {
         try {
-           const response =await createPost(post);
+           const response = await createPost(formData);
            navigate(`/posts/${response.id}`)
         } catch (error) {
             console.error("Failed to create post" ,error);
         }
     };
     return (
-        <div>
-        <h2>New Post to Add</h2>
-            <form onSubmit={handleSubmit}>
-                <div>
-                    <label htmlFor="titleInput">Title: </label>
-                    <input
-                        id="titleInput"
-                        type="text"
-                        value={title}
-                        onChange={(e) => setTitle(e.target.value)}
-                        required
-                    />
-                </div>
-                <div>
-                    <label htmlFor="bodyInput">Body: </label>
-                    <textarea
-                        id="bodyInput"
-                        type="text"
-                        value={body}
-                        onChange={(e) => setBody(e.target.value)}
-                        required
-                    />
-                </div>
-                <div>
-                    <button type="submit">Create Post</button>
-                </div>
-            </form>
-        </div>
+        <PostForm
+            headerText="CREATE NEW POST"
+            onSubmit={handleCreateSubmit}
+            buttonText="Create Post"
+        />
     );
 }
 
